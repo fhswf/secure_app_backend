fc78e4255bb3ec205376eac919c62ef616fd6d71
Christian Gawron <gawron.christian@fh-swf.de>
2021-01-04 13:32:27 +0100

    changes for Docker

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const port = 3000
 const fs = require('fs');
 const date = new Date;
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 //Route zur API deklarieren
-const messageRouter = require('./routes/messages')
-app.use('/messages', messageRouter)
 
 // Static Files deklarieren
 app.use(express.static('public'))
@@ -21,7 +19,7 @@ app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // views deklarieren
@@ -29,28 +27,28 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/api/public/uhrzeit', (req, res) => {
-   res.json({     
-     jahr: date.getUTCFullYear(),
-     monat: date.getUTCMonth() + 1,
-     tag: date.getUTCDate(),
-     stunden: date.getUTCHours()+1,
-     minuten: date.getUTCMinutes(),
-     sekunden: date.getUTCSeconds()
-   });
+  res.json({
+    jahr: date.getUTCFullYear(),
+    monat: date.getUTCMonth() + 1,
+    tag: date.getUTCDate(),
+    stunden: date.getUTCHours() + 1,
+    minuten: date.getUTCMinutes(),
+    sekunden: date.getUTCSeconds()
+  });
 })
 
 app.get('', (req, res) => {
-  res.render('index', {text: 'Startseite des Backends'})
+  res.render('index', { text: 'Startseite des Backends' })
 })
 
 //Fehlermeldungen für die Statuscodes 401 und 404
-app.use(function(req, res, next) {
-    res.status(404).send('Diese Seite existiert nicht!');
-  });
-
-app.use(function(req, res, next) {
-    res.status(401).send('Fehlende Rechte für diesen Bereich!');
+app.use(function (req, res, next) {
+  res.status(404).send('Diese Seite existiert nicht!');
 });
-  
+
+app.use(function (req, res, next) {
+  res.status(401).send('Fehlende Rechte für diesen Bereich!');
+});
+
 // Konsolenausgaben
 app.listen(port, () => console.info(`Backend laueft auf Port ${port}`))
