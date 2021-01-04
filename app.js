898efd04e0b5bbeede4294f1f5d02c48574d7082
Christian Gawron <gawron.christian@fh-swf.de>
2021-01-04 13:57:01 +0100

    Docker (#1)
    * adding Dockerfile
    
    * changes for Docker

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+<<<<<<< HEAD
 var jwt = require('express-jwt');
 var jwks = require('jwks-rsa');
 
@@ -25,10 +26,15 @@ var jwtCheck = jwt({
   issuer: 'https://dev-dqjyvzmz.eu.auth0.com/',
   algorithms: ['RS256']
  });
+=======
+const fs = require('fs');
+//let date = new Date;
+>>>>>>> bc41aab... Docker (#1)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+<<<<<<< HEAD
 
 /**
  * Implementiert einen öffentlichen API-Endpunkt, der beim Aufruf das aktuelle Datum mit aktueller Uhrzeit ausgibt.
@@ -76,4 +82,49 @@ app.use(function (req, res, next) {
 /**
  * Konsolenausgabe
  */
+=======
+//Route zur API deklarieren
+
+// Static Files deklarieren
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// views deklarieren
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('/api/public/uhrzeit', (req, res) => {
+  let date = new Date();
+  res.json({
+    jahr: date.getUTCFullYear(),
+    monat: date.getUTCMonth() + 1,
+    tag: date.getUTCDate(),
+    stunden: date.getUTCHours() + 1,
+    minuten: date.getUTCMinutes(),
+    sekunden: date.getUTCSeconds()
+  });
+})
+
+app.get('/index.html', (req, res) => {
+  res.render('index', { text: 'Startseite des Backends' })
+})
+
+
+//Fehlermeldungen für die Statuscodes 401 und 404
+app.use(function (req, res, next) {
+  res.status(404).send('Diese Seite existiert nicht!');
+});
+
+app.use(function (req, res, next) {
+  res.status(401).send('Fehlende Rechte für diesen Bereich!');
+});
+
+// Konsolenausgaben
+>>>>>>> bc41aab... Docker (#1)
 app.listen(port, () => console.info(`Backend laueft auf Port ${port}`))
