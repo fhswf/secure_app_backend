bd8525146dafedb205b0c40c3468c820b89c95d3
Marcel Hopp <m94_hopp@web.de>
2021-01-05 17:39:14 +0100

    API angepasst

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-// Imports
+/**
+ * Backend zur SecureApp.
+ * Diese Datei implementiert den Webserver mit Hilfe von NodeJS.
+ * 
+ * @author Marcel Hopp 
+ */
+
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-const fs = require('fs');
-const date = new Date;
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
-//Route zur API deklarieren
-const messageRouter = require('./routes/messages')
-app.use('/messages', messageRouter)
 
 // Static Files deklarieren
 app.use(express.static('public'))
@@ -21,7 +21,7 @@ app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // views deklarieren
@@ -29,28 +29,41 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/api/public/uhrzeit', (req, res) => {
-   res.json({     
-     jahr: date.getUTCFullYear(),
-     monat: date.getUTCMonth() + 1,
-     tag: date.getUTCDate(),
-     stunden: date.getUTCHours()+1,
-     minuten: date.getUTCMinutes(),
-     sekunden: date.getUTCSeconds()
-   });
+  let date = new Date();
+
+  var wochentage_array = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+  var wochentag = wochentage_array[date.getDay()];
+
+  year = date.getFullYear().toString();
+  month = (date.getMonth() + 1).toString().padStart(2, "0");
+  day   = date.getDate().toString().padStart(2, "0");
+  hours = date.getHours().toString().padStart(2, "0");
+  minutes = date.getMinutes().toString().padStart(2, "0");
+  seconds = date.getSeconds().toString().padStart(2, "0");
+  
+  res.json({
+    wochentag: wochentag,
+    tag: day,
+    monat: month,
+    jahr: year, 
+    stunden: hours, 
+    minuten: minutes,
+    sekunden: seconds
+  });
 })
 
-app.get('', (req, res) => {
-  res.render('index', {text: 'Startseite des Backends'})
+app.get('/index.html', (req, res) => {
+  res.render('index', { text: 'Startseite des Backends' })
 })
 
 //Fehlermeldungen für die Statuscodes 401 und 404
-app.use(function(req, res, next) {
-    res.status(404).send('Diese Seite existiert nicht!');
-  });
-
-app.use(function(req, res, next) {
-    res.status(401).send('Fehlende Rechte für diesen Bereich!');
+app.use(function (req, res, next) {
+  res.status(404).send('Diese Seite existiert nicht!');
 });
-  
+
+app.use(function (req, res, next) {
+  res.status(401).send('Fehlende Rechte für diesen Bereich!');
+});
+
 // Konsolenausgaben
 app.listen(port, () => console.info(`Backend laueft auf Port ${port}`))
