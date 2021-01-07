0015afe0698cfeee001c788ae9b76e71e3669c6d
Marcel Hopp <m94_hopp@web.de>
2021-01-07 16:46:38 +0100

    Öffentlichen und Privaten API-Endpunkt hinzugefügt.

--- app.js
+++ app.js
@@ -6,40 +6,46 @@
  */
 
 const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+/**
+ * Implementiert die Auth0.com-Funktionen.
+ */
+
+var jwtCheck = jwt({
+   secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://dev-dqjyvzmz.eu.auth0.com/.well-known/jwks.json'
+ }),
+  audience: 'http://127.0.0.1:3000/api/private', 
+  issuer: 'https://dev-dqjyvzmz.eu.auth0.com/',
+  algorithms: ['RS256']
+ });
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// Static Files deklarieren
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-// views deklarieren
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-app.get('/api/public/uhrzeit', (req, res) => {
+/**
+ * Implementiert einen öffentlichen API-Endpunkt, der beim Aufruf das aktuelle Datum mit aktueller Uhrzeit ausgibt.
+ */
+app.get('/api/public', (req, res) => {
   let date = new Date();
 
-  var wochentage_array = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
-  var wochentag = wochentage_array[date.getDay()];
+  let wochentage_array = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+  let wochentag = wochentage_array[date.getDay()];
 
-  year = date.getFullYear().toString();
-  month = (date.getMonth() + 1).toString().padStart(2, "0");
-  day   = date.getDate().toString().padStart(2, "0");
-  hours = date.getHours().toString().padStart(2, "0");
-  minutes = date.getMinutes().toString().padStart(2, "0");
-  seconds = date.getSeconds().toString().padStart(2, "0");
+  let year = date.getFullYear().toString();
+  let month = (date.getMonth() + 1).toString().padStart(2, "0");
+  let day   = date.getDate().toString().padStart(2, "0");
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let seconds = date.getSeconds().toString().padStart(2, "0");
   
   res.json({
     wochentag: wochentag,
@@ -49,20 +55,21 @@ app.get('/api/public/uhrzeit', (req, res) => {
     stunden: hours, 
     minuten: minutes,
     sekunden: seconds
-  });
+    });
 })
 
-app.get('/index.html', (req, res) => {
-  res.render('index', { text: 'Startseite des Backends' })
-})
+/**
+ * Implementiert einen privaten API-Endpunkt. Um diesen aufzurufen, ist ein JWT nötig.
+ */
+app.get('/api/private', jwtCheck, function(req, res) {
+  res.json({
+    message: 'Hello World vom privaten Endpunkt des Backends!'
+  });
+});
 
 //Fehlermeldungen für die Statuscodes 401 und 404
 app.use(function (req, res, next) {
   res.status(404).send('Diese Seite existiert nicht!');
-});
-
-app.use(function (req, res, next) {
-  res.status(401).send('Fehlende Rechte für diesen Bereich!');
 });
 
 // Konsolenausgaben
